1bc27cac66b0ee2eb2831a9ca873370ef40165a2
wenxin0708 <wenxin0708@gmail.com>
2018-03-19 12:20:27 +0800

    3/19-1

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-import bavel from 'rollup-plugin-babel'
+
+import babel from 'rollup-plugin-babel';
 
 export default {
-    input : 'src/index.js',
-	output : {
-	    file : 'out/ulog.js',
-        format ：'life',
-        sourcemap : 'true',
-    },
-    plugins: [
-        babel ({
-            exclude: 'node_modules/**',
-        })
-    ],
-};	
+  input: 'src/index.js',
+  output: {
+    file: 'out/ulog.js',
+    format: 'iife',
+    sourcemap: 'true',
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    })
+  ],
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
-window.addEventListener('load', () => {
-   let h1 = document.createElement('h1');
+/**
+ *  @file       index.js
+ *  @brief      The entry point of ULog.
+ *  @author     Yiwei Chiao (ywchiao@gmail.com)
+ *  @date       03/12/2018 created.
+ *  @date       03/18/2018 last modified.
+ *  @version    0.1.0
+ *  @since      0.1.0
+ *  @copyright  MIT, © 2018 Yiwei Chiao
+ *  @details
+ *
+ *  The entry point of ULog.
+ */
+'use strict';
 
-   h1.textContent = 'HelloWorld!';
-   
-   document.body.appendChild(h1);
+import Blog from './blog/blog.js';
+
+window.addEventListener('load', () => {
+  let blog = Blog();
+
+  document.getElementsByTagName('html')[0].style.height = '100vh';
+
+  document.body.style.height = '100vh';
+  document.body.style.margin = '0';
+
+  document.body.appendChild(blog.node);
 });
+
+// index.js
